d8cbbb284aa1081b54905bd887e32b7a9209218a
nk-akun <nk_akun@163.com>
2021-11-17 19:44:35 +0800

    feat: add 11

--- 11.盛最多水的容器.go
+++ 11.盛最多水的容器.go
@@ -6,9 +6,39 @@
 
 package main
 
+import "fmt"
+
 // @lc code=start
 func maxArea(height []int) int {
+	ans := 0
+	l, r := 0, len(height)-1
+	for l < r {
+		ans = max(ans, min(height[l], height[r])*(r-l))
+		if height[l] > height[r] {
+			r--
+		} else {
+			l++
+		}
+	}
+	return ans
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
+
+func main() {
+	fmt.Println(maxArea([]int{1, 2, 1}))
+}
