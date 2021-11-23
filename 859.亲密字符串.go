83302a1ed5e1fd1a34ab0ef32eb4e77571b07bad
nk-akun <nk_akun@163.com>
2021-11-23 17:39:33 +0800

    feat: add 859

---
+++ 859.亲密字符串.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=859 lang=golang
+ *
+ * [859] 亲密字符串
+ */
+
+// 两种合法情况，"aac""aac"或"abc""acb"，其他情况均不合法
+
+package main
+
+import "fmt"
+
+// @lc code=start
+func buddyStrings(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
+
+	uni := []int{}
+	mp := map[byte]int{}
+	for i := range s {
+		if s[i] != goal[i] {
+			uni = append(uni, i)
+		}
+		mp[s[i]]++
+	}
+
+	if len(uni) == 0 {
+		for _, v := range mp {
+			if v >= 2 {
+				return true
+			}
+		}
+		return false
+	} else if len(uni) == 2 {
+		if s[uni[0]] == goal[uni[1]] && s[uni[1]] == goal[uni[0]] {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+// @lc code=end
+
+func main() {
+	fmt.Println(buddyStrings("a", "c"))
+}
