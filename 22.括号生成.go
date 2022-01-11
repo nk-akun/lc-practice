ada34e1bf0071f23b53cd7bdf26a0c24d19e4783
nk-akun <nk_akun@163.com>
2022-01-11 23:18:22 +0800

    feat: add 22,深搜+剪枝

--- 22.括号生成.go
+++ 22.括号生成.go
@@ -4,6 +4,8 @@
  * [22] 括号生成
  */
 
+// 深搜+剪枝
+
 package main
 
 // @lc code=start
@@ -28,10 +30,13 @@ func dfs(pos, sum, n int) {
 		return
 	}
 
+	// 如果剩余的都是有括号，能否抵掉当前的左括号
 	if sum < (n - pos) {
 		ket[pos] = '('
 		dfs(pos+1, sum+1, n)
 	}
+
+	// 只有有左括号时，才能有有括号
 	if sum > 0 {
 		ket[pos] = ')'
 		dfs(pos+1, sum-1, n)
