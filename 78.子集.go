ec22b1a400de757a4643f3f5bf9c2020e639f19c
nk-akun <nk_akun@163.com>
2022-03-19 15:42:22 +0800

    feat: add 78,二进制,回溯

---
+++ 78.子集.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=golang
+ *
+ * [78] 子集
+ */
+
+//  10/10 cases passed (0 ms)
+//  Your runtime beats 100 % of golang submissions
+//  Your memory usage beats 57.03 % of golang submissions (2.1 MB)
+
+package main
+
+// @lc code=start
+func subsets(nums []int) [][]int {
+	ans := make([][]int, 1<<len(nums))
+	for i := 0; i < (1 << (len(nums))); i++ {
+		q := make([]int, 0)
+		for j := 0; j < len(nums); j++ {
+			// fmt.Println(i, j)
+			if (i & (1 << j)) > 0 {
+				q = append(q, nums[j])
+			}
+			// fmt.Println(q)
+		}
+		ans[i] = q
+	}
+	return ans
+}
+
+// @lc code=end
+
+// func main() {
+// 	fmt.Println(subsets([]int{1}))
+// }
