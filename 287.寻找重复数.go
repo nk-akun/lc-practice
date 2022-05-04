69f7fdae1fd0f27db348ce32057a99774ae39bd3
nk-akun <nk_akun@163.com>
2022-05-04 22:14:53 +0800

    feat: add 287,链表,快慢指针

--- 287.寻找重复数.go
+++ 287.寻找重复数.go
@@ -10,7 +10,7 @@ package main
 // Your runtime beats 70.67 % of golang submissions
 // Your memory usage beats 82.82 % of golang submissions (8.2 MB)
 
-// 快慢指针
+// 快慢指针,时间O(N),空间O(1)
 // 因为数字都在[1,n]范围内，所以如果将数字nums[i]和下标nums[i]建立有向边
 // 那么重复数字所指向的下标入度肯定大于1，即出现了环
 // 所以该题就变为在单向链表中寻找环的入口
